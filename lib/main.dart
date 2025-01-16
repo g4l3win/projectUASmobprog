8b18b97c2545b4a53d2f439fd0443731805d1a29
g4l3win <144231516+g4l3win@users.noreply.github.com>
2025-01-16 17:50:41 +0700

    tambahin notifikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:quizdb/screens/SignIn.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
-void main() => runApp(MyApp());
+//fungsi main itu titik masuk aplikasi flutter.dipanggil saat aplikasi lagi dijalankan
+//run(MyApp()) memulai aplikasi flutter dengan widget root myApp
+void main() {
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'basic_channel',
+        channelName: 'Basic notifications',
+        channelDescription: 'basic_channelDescription',
+      ),
+    ],
+    debug: true,
+  );
+  // Request notification permission if it's not already granted
+  requestNotificationPermission();
+  runApp(MyApp());
+}
 
+// Function to request notification permission from the user
+void requestNotificationPermission() async {
+  bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowed) {
+    // Show the permission request dialog
+    AwesomeNotifications().requestPermissionToSendNotifications();
+  }
+}
+
+//MyApp ADALAH root widget dari aplikasi
+//root widget adalah widget pertama yang dibuat dalam hierarki widget
+//MyApp mengembalikan widget MaterialApp yang mengatur tema, judul, halaman utama aplikasi
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kuis Interaktif',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Signin(),
+      home: Signin(), //halaman home/ utama SignIn()
       debugShowCheckedModeBanner: false, // Disable the debug banner
     );
   }
